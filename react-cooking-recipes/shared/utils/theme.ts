ec66a3222dd50dab5e3fda958fc1e66823db9391
Natasza ASzatan <natiye.nb@gmai.com>
2025-01-14 18:45:30 +0100

    added theme

---
+++ react-cooking-recipes/shared/utils/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#d5f2e3",
+      main: "#8ecfb3",
+      dark: "#5d9d83",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#e7d9c4",
+      main: "#d4c4a8",
+      dark: "#b9a688",
+      contrastText: "#ffffff",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 480,
+      md: 768,
+      lg: 992,
+      xl: 1279,
+    },
+  },
+  typography: {
+    fontFamily: "Arial, sans-serif",
+    fontSize: 16,
+  },
+});
+
+export default theme;
