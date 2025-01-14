6575955dbc77130fe5047522961e986e28ca1b24
Natasza ASzatan <natiye.nb@gmai.com>
2025-01-14 19:05:15 +0100

    RecipeCard component displays all info for a recipe

--- react-cooking-recipes/src/components/RecipeCard/index.tsx
+++ react-cooking-recipes/src/components/RecipeCard/index.tsx
@@ -1,5 +1,12 @@
 import { FunctionComponent } from "react";
 import { Recipe } from "../../../shared/types/Recipe.ts";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -8,7 +15,25 @@ interface RecipeCardProps {
 export function RecipeCard({ recipe }: RecipeCardProps) {
   return (
     <div>
-      <p>{recipe.title}</p>
+      <div>
+        <p>picture placeholder</p>
+      </div>
+      <div>
+        <h2>{recipe.title}</h2>
+        <h5>{recipe.allergens}</h5>
+        <div>
+          <List>
+            {recipe.cookingSteps.map((value, index) => (
+              <ListItem key={index}>
+                <ListItemButton>
+                  <ListItemIcon>{index + 1}.</ListItemIcon>
+                  <ListItemText>{value}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </div>
     </div>
   );
 }
